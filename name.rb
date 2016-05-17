2b3d505d775d2caf7f56968a002d383c2c9d5262
Alexander Ross <alexandersmithross@gmail.com>
2016-05-17 14:58:13 -0700

    name game for family

---
+++ name.rb
@@ -0,0 +1,37 @@
+puts "what is your name?"
+name=gets.chomp
+
+if name=='Lily' 
+
+  then puts "Wow I am so happy to meet you "+name+"! You are Alexanders favorite wife! He told me
+so himself! We talk alot about you becuase your husband just cant stop telling
+me how much he loves you.  Also he told me not to make that joke about the favorite wife
+but I couldnt resist!"
+end 
+
+
+if name == 'Alexander'
+
+  then puts "Hi " + name + " the Great, I have heard you are the master of my universe! All this time 
+we have spent together where I watched you flounder around like you knew nothing and here 
+you are master of my universe and I didn't even know!"
+end
+if name == 'Alex'
+  then puts "Hi " + name + ", I have heard you are the master of my universe! All this time we have spent 
+together where I watched you flounder around like you knew nothing and here you are master
+of my universe and I didn't even know!"
+end
+if name == 'Brendan'
+
+  then puts "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
+just how much he loves you and how he is so proud of you."
+end
+if name == 'Bren'
+  then puts "Hello " + name + " it is very nice to meet you. I have heard alot about you. Your dad told me 
+just how much he loves you and how he is so proud of you."
+end
+
+if name == 'Autumn'
+  then puts "Hello " + name + " I am so impressed you know your name before you could talk! Wait a second!
+Who is this! This can not be Autumn since I was told she is not even 1 year old yet!"
+end
