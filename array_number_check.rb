a6925653cb298901f74b798aaf0dc03eab83da9c
Alexander Ross <alexandersmithross@gmail.com>
2016-05-20 07:00:09 -0700

    array exercises

---
+++ array_number_check.rb
@@ -0,0 +1,8 @@
+# array_number_check.rb
+
+arr=[1,3,5,7,9,11]
+number = 3
+
+if arr.include?(number)
+  puts "#{number} is included in the array"
+end
---
+++ iterates_over_array.rb
@@ -0,0 +1,12 @@
+# iterates_over_array.rb
+
+
+arr = [1, 2, 3, 4, 5]
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
